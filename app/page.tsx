efa6469277611aacc4b72fe4b137c8ce392b52e0
bestinksalesman <bestinksalesman@gmail.com>
2025-11-03 21:53:54 +0800

    Input icons: fix vertical centering by setting yOffset to 0 (clip/camera)

--- app/page.tsx
+++ app/page.tsx
@@ -2396,7 +2396,7 @@ export default function Home() {
                   onFocus={(e) => { (e.currentTarget as HTMLButtonElement).style.outline = 'none'; e.currentTarget.style.boxShadow = '0 0 0 2px rgba(0,122,255,0.25)'; e.currentTarget.style.background = '#f3f4f6'; }}
                   onBlur={(e) => { e.currentTarget.style.boxShadow = 'none'; e.currentTarget.style.background = 'transparent'; }}
                 >
-                  <ClipIcon size={isMobile ? 22 : 24} yOffset={2} />
+                  <ClipIcon size={isMobile ? 22 : 24} yOffset={0} />
                 </button>
                 {isMobile && (
                 <button
@@ -2422,7 +2422,7 @@ export default function Home() {
                     onFocus={(e) => { (e.currentTarget as HTMLButtonElement).style.outline = 'none'; e.currentTarget.style.boxShadow = '0 0 0 2px rgba(0,122,255,0.25)'; e.currentTarget.style.background = '#f3f4f6'; }}
                     onBlur={(e) => { e.currentTarget.style.boxShadow = 'none'; e.currentTarget.style.background = 'transparent'; }}
                   >
-                    <CameraIcon size={isMobile ? 22 : 24} yOffset={2} />
+                    <CameraIcon size={isMobile ? 22 : 24} yOffset={0} />
                   </button>
                 )}
               </div>
