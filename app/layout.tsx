f1b5b5bb925cb2cd830255d0421531340671f41d
bestinksalesman <bestinksalesman@gmail.com>
2025-11-04 18:34:57 +0800

    Disable dark mode: force light color scheme globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
+    <html lang="ja" style={{ colorScheme: 'light' }}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
